fc09c5ffd1922793b505e81abdb1b15ad0fa1d56
dmytro-chushko <dmytro.chushko@gmail.com>
2026-04-21 23:31:25 +0300

    turborepo init

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,56 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+import onlyWarn from "eslint-plugin-only-warn";
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config}
+ * */
+export const config = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      turbo: turboPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "padding-line-between-statements": [
+        "error",
+        { blankLine: "always", prev: "*", next: "if" },
+        { blankLine: "always", prev: "*", next: "return" },
+      ],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      onlyWarn,
+    },
+  },
+  {
+    ignores: ["dist/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+  },
+];
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,4 @@
+export { Card } from "./card.js";
+export { Gradient } from "./gradient.js";
+export { TurborepoLogo } from "./turborepo-logo.js";
+export { Button, buttonVariants } from "./components/ui/button.js";
