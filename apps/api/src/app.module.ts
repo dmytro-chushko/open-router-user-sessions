2d92e68ecb335fe1c3df24ab5709fa6eaf7635b1
dmytro-chushko <dmytro.chushko@gmail.com>
2026-04-23 00:58:33 +0300

    Add swagger and app config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ApiContractController } from './api-contract.controller';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CommonModule],
   controllers: [ApiContractController],
   providers: [],
 })
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,51 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+import { generateOpenApi } from '@ts-rest/open-api';
+import * as swaggerUi from 'swagger-ui-express';
+import { contract } from '@repo/api-contracts';
 import { AppModule } from './app.module';
+import { AppConfigService } from './common/app-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+
+  const appConfig = app.get(AppConfigService);
+
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: appConfig.webOrigins,
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3002);
+
+  const openApiDocument = generateOpenApi(
+    contract,
+    {
+      info: {
+        title: 'API',
+        version: '1.0.0',
+      },
+      servers: [
+        {
+          url: appConfig.apiUrl,
+          description: 'API Server',
+        },
+      ],
+    },
+    { setOperationId: true },
+  );
+
+  app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(openApiDocument, {
+      customCss: '.swagger-ui .topbar { display: none }',
+      swaggerOptions: {
+        persistAuthorization: true,
+        displayRequestDuration: true,
+      },
+    }),
+  );
+
+  await app.listen(appConfig.port);
 }
 void bootstrap();
