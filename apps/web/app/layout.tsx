fa073e2df1cca8dd7923fb4c9b67e39c375a1f08
dmytro-chushko <dmytro.chushko@gmail.com>
2026-05-04 23:50:42 +0300

    Fix theme switching

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { ReactNode } from "react";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return children;
 }
