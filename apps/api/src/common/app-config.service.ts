ff9526fd9d20849c3136b875238c4db82c6b5405
dmytro-chushko <dmytro.chushko@gmail.com>
2026-05-10 00:43:51 +0300

    Add variables to the app-config

--- apps/api/src/common/app-config.service.ts
+++ apps/api/src/common/app-config.service.ts
@@ -34,4 +34,79 @@ export class AppConfigService {
       .map((s) => s.trim())
       .filter((s) => s.length > 0);
   }
+
+  get sessionSecret(): string {
+    return this.config.get<string>('SESSION_SECRET') ?? 'session-secret';
+  }
+
+  get sessionCookoeName(): string {
+    return this.config.get<string>('SESSION_COOKIE_NAME') ?? 'session';
+  }
+
+  get sessionCookieMaxAge(): number {
+    return (
+      this.config.get<number>('SESSION_COOKIE_MAX_AGE') ?? 60 * 60 * 24 * 30
+    );
+  }
+
+  get cookieSameSite(): 'lax' | 'strict' | 'none' {
+    return (
+      this.config.get<'lax' | 'strict' | 'none'>('COOKIE_SAME_SITE') ?? 'lax'
+    );
+  }
+
+  get cookieSecure(): boolean {
+    return this.config.get<boolean>('COOKIE_SECURE') ?? false;
+  }
+
+  get googleOAuth(): {
+    clientId: string;
+    clientSecret: string;
+    callbackUrl: string;
+  } {
+    return {
+      clientId: this.config.get<string>('GOOGLE_CLIENT_ID') ?? '',
+      clientSecret: this.config.get<string>('GOOGLE_CLIENT_SECRET') ?? '',
+      callbackUrl: this.config.get<string>('GOOGLE_CALLBACK_URL') ?? '',
+    };
+  }
+
+  get githubOAuth(): {
+    clientId: string;
+    clientSecret: string;
+    callbackUrl: string;
+  } {
+    return {
+      clientId: this.config.get<string>('GITHUB_CLIENT_ID') ?? '',
+      clientSecret: this.config.get<string>('GITHUB_CLIENT_SECRET') ?? '',
+      callbackUrl: this.config.get<string>('GITHUB_CALLBACK_URL') ?? '',
+    };
+  }
+
+  get webAppUrl(): string {
+    return this.config.get<string>('WEB_APP_URL') ?? 'http://localhost:3001';
+  }
+
+  get emailFrom(): string {
+    return (
+      this.config.get<string>('EMAIL_FROM') ??
+      'no-reply@open-router-sessions.com'
+    );
+  }
+
+  get resendApiKey(): string {
+    return this.config.get<string>('RESEND_API_KEY') ?? '';
+  }
+
+  get enableAdminSeed(): boolean {
+    return this.config.get<boolean>('ENABLE_ADMIN_SEED') ?? false;
+  }
+
+  get adminEmail(): string {
+    return this.config.get<string>('ADMIN_EMAIL') ?? '';
+  }
+
+  get adminSeedPassword(): string {
+    return this.config.get<string>('ADMIN_SEED_PASSWORD') ?? '';
+  }
 }
