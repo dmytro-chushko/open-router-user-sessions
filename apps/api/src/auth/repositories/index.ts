14c4eca43ba1b276199f2a6d24d3cf549e8d66be
dmytro-chushko <dmytro.chushko@gmail.com>
2026-05-16 23:45:59 +0300

    Add reset password service

--- apps/api/src/auth/repositories/index.ts
+++ apps/api/src/auth/repositories/index.ts
@@ -1,3 +1,4 @@
 export * from '@/auth/repositories/email-verification-tokens.repository';
+export * from '@/auth/repositories/password-reset-tokens.repository';
 export * from '@/auth/repositories/sessions.repository';
 export * from '@/auth/repositories/users.repository';
